fc0a9eda2d11b626c0ab72b2bca3b192120df124
sonsungjong <tjdwhd70599@gmail.com>
2023-04-27 16:05:32 +0900

    WPF Application .net 7

--- WpfCtrl02StackPanel/MainWindow.xaml.cs
+++ WpfCtrl02StackPanel/MainWindow.xaml.cs
@@ -13,17 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-// net7 WPF Application
-
-/*
- <<WPF Application 선택하기>>
-
-결론적으로, 새로운 WPF 프로젝트를 시작할 때 "WPF Application"을 선택하는 것이 좋습니다. 
-이를 통해 최신 기능, 성능 개선 및 지원을 활용할 수 있습니다. 
-하지만 기존 프로젝트와 호환성이 중요한 경우, "WPF App (.NET Framework)"를 선택할 수 있습니다. 
-*/
-
-namespace WPFApplication
+namespace WpfCtrl02StackPanel
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
