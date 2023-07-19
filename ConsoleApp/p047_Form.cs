ddb4e6829b3bfab4607baf429a8c893373bdd432
sonsungjong <tjdwhd70599@gmail.com>
2023-07-20 08:31:16 +0900

    Dotnet Console

---
+++ ConsoleApp/p047_Form.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace ConsoleApp
+{
+    public class P047
+    {
+        public static void Main047(string[] args)
+        {
+            Form fm;
+            fm = new Form();
+
+            fm.Text = "Hello C#!";
+            Application.Run(fm);
+        }
+    }
+}
