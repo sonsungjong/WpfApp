2fea87712cf9aafddafbfaa2ba9749d70c14447f
sonsungjong <tjdwhd70599@gmail.com>
2023-08-02 21:59:13 +0900

    C# Design Pro MVVM WPF 2

---
+++ ModernDashboard/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ModernDashboard
+{
+    /*
+     RelayCommand allows you to inject the command's logic via delegates passed into its constructor.
+    This method enables ViewModel classes to implement commands in a concise manner.
+
+    RelayCommand를 사용하면 생성자에 전달된 delegates를 통해 명령의 논리를 주입할 수 있습니다.
+     이 방법을 사용하면 ViewModel 클래스가 간결한 방식으로 명령을 구현할 수 있습니다.
+     */
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public RelayCommand(Action<object> execute)
+        {
+            this.execute = execute;
+            canExecute = null;
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /*
+         CanExecuteChanged delegates the event subscription to the CommandManager.RequerySuggested event.
+        This ensures that the WPF commanding infrastructure asks all RelayCommand objects if they can execute whenever it asks the built-in commands.
+        CanExecuteChanged는 이벤트 구독을 CommandManager.RequerySuggested 이벤트에 위임합니다.
+         이렇게 하면 WPF 명령 인프라가 기본 제공 명령을 요청할 때마다 실행할 수 있는지 여부를 모든 RelayCommand 개체에 묻습니다.
+         */
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || CanExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ ModernDashboard/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Input;
+using ModernDashboard.Model;
+
+namespace ModernDashboard.ViewModel
+{
+    public class NavigationViewModel : INotifyPropertyChanged
+    {
+        // CollectionViewSource enables XAML code to set the commonly used CollectionView properties, passing these settings to the underlying view.
+        private CollectionViewSource MenuItemsCollection;
+
+        // ICollectionView enables collections to have the functionalities of current record management, custom sorting, filtering, and grouping.
+        public ICollectionView SourceCollection => MenuItemsCollection.View;
+
+        public NavigationViewModel()
+        {
+            // ObservableCollection represents a dynamic data collection that provides notifications when items get added, removed, or when the whole list is refreshed.
+            ObservableCollection<MenuItems> menuItems = new ObservableCollection<MenuItems>
+            {
+                new MenuItems {MenuName = "Home", MenuImage = @"Assets/Home_Icon.png"},
+                new MenuItems {MenuName = "Desktop", MenuImage = @"Assets/Desktop_Icon.png"},
+                new MenuItems {MenuName = "Documents", MenuImage = @"Assets/Document_Icon.png"},
+                new MenuItems {MenuName = "Downloads", MenuImage = @"Assets/Download_Icon.png"},
+                new MenuItems {MenuName = "Pictures", MenuImage = @"Assets/Images_Icon.png"},
+                new MenuItems {MenuName = "Music", MenuImage = @"Assets/Music_Icon.png"},
+                new MenuItems {MenuName = "Movies", MenuImage = @"Assets/Movies_Icon.png"},
+                new MenuItems {MenuName = "Trash", MenuImage = @"Assets/Trash_Icon.png"}
+            };
+
+            MenuItemsCollection = new CollectionViewSource { Source = menuItems };
+            MenuItemsCollection.Filter += MenuItems_Filter;
+        }
+
+        // Implement interface member for INotifyPropertyChanged.
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        // Text Search Filter.
+        private string filterText;
+        public string FilterText
+        {
+            get => filterText;
+            set
+            {
+                filterText = value;
+                MenuItemsCollection.View.Refresh();
+                OnPropertyChanged("FilterText");
+            }
+        }
+
+        private void MenuItems_Filter(object sender, FilterEventArgs e)
+        {
+            if (string.IsNullOrEmpty(FilterText))
+            {
+                e.Accepted = true;
+                return;
+            }
+
+            MenuItems _item = e.Item as MenuItems;
+            if (_item.MenuName.ToUpper().Contains(FilterText.ToUpper()))
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
+        }
+    }
+}
