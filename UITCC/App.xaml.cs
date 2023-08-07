d7d7c14231065d76c31ded5681cc221593b7761b
sonsungjong <tjdwhd70599@gmail.com>
2023-08-07 17:00:11 +0900

    wpf app

---
+++ UITCC/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UITCC
+{
+    public partial class App : Application
+    {
+        protected void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            /*
+            var login_view = new LoginView();
+            login_view.Show();
+            login_view.IsVisibleChanged += (s, ev) =>
+            {
+                if(login_view.IsVisible == false && login_view.IsLoaded)
+                {
+                    var select_mode_view = new select_mode_view();
+                    select_mode_view.Show();
+                    login_view.Close();
+                }
+            };
+            */
+        }
+    }
+}
