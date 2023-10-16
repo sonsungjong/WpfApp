881312043a5a3ca1c8a4806e6dd281c219d0f873
sonsungjong <tjdwhd70599@gmail.com>
2023-10-17 05:56:46 +0900

    rj

---
+++ MVVM_RJ/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MVVM_RJ.View;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM_RJ
+{
+    public partial class App : Application
+    {
+        protected void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            var loginView = new LoginView();
+            loginView.Show();
+            loginView.IsVisibleChanged += (s, ev) =>
+            {
+                if(loginView.IsVisible == false && loginView.IsLoaded)
+                {
+                    var mainView = new MainView();
+                    mainView.Show();
+                    loginView.Close();
+                }
+            };
+        }
+    }
+}
