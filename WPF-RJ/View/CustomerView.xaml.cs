1dd12d4ef60e6ec1f3a8723ba450157efaec7477
sonsungjong <tjdwhd70599@gmail.com>
2023-10-31 15:19:00 +0900

    데이터 그리드 스크롤은... 일단 보류

--- WPF-RJ/View/CustomerView.xaml.cs
+++ WPF-RJ/View/CustomerView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,29 @@ namespace WPF_RJ.View
     /// </summary>
     public partial class CustomerView : UserControl
     {
+
         public CustomerView()
         {
             InitializeComponent();
+        }
+
+        private void DataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            e.Column.CanUserSort = false;
+            e.Column.CanUserResize = false;
+
+            DataGridTextColumn textColumn = e.Column as DataGridTextColumn;
+            if (textColumn != null)
+            {
+                textColumn.ElementStyle = new Style(typeof(TextBlock))
+                {
+                    Setters = {
+                new Setter(TextBlock.ForegroundProperty, Brushes.White),
+                new Setter(TextBlock.TextAlignmentProperty, TextAlignment.Center),
+                new Setter(TextBlock.BackgroundProperty, Brushes.Gold)
+            }
+                };
+            }
         }
     }
 }
--- WPF-RJ/ViewModel/CustomerViewModel.cs
+++ WPF-RJ/ViewModel/CustomerViewModel.cs
@@ -1,26 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using WPF_RJ.Model;
 
 namespace WPF_RJ.ViewModel
 {
     public class CustomerViewModel : ViewModelBase
     {
-        public ICommand ImageButtonCommand { get; }
+        private ObservableCollection<TableDataModel> m_table_data_list;
 
+        public ObservableCollection<TableDataModel> TableDataList
+        {
+            get { return m_table_data_list; }
+            set
+            {
+                if(m_table_data_list != value)
+                {
+                    m_table_data_list = value;
+                    OnPropertyChanged(nameof(TableDataList));
+                }
+            }
+        }
+
+        // 버튼 ICommand 생성
+        public ICommand ImageButtonCommand { get; set; }
+
+        // 생성자
         public CustomerViewModel()
         {
-            ImageButtonCommand = new ViewModelCommand(ExecuteImageButtonCommand);
+            // ICommand 적용
+            ImageButtonCommand = new ViewModelCommand(ExecuteImageButtonCommand);           // 다이아몬드 버튼 (임시)
 
+            // 데이터그리드 생성
+            TableDataList = new ObservableCollection<TableDataModel>();
+
+            // 빈 행 20개
+            for(int i = 0; i < 20; i++)
+            {
+                TableDataList.Add(new TableDataModel { A = "안녕", B = "하세요", C = "반갑" });
+            }
         }
 
         private void ExecuteImageButtonCommand(object obj)
         {
-            MessageBox.Show("hello");
+            TableDataModel newData = new TableDataModel { A = "에이", B = "삐", C = "씨" };
+
+            TableDataList.Add(newData);
         }
     }
 }
