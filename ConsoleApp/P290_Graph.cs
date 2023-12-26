4b2ab52e0d17ea621fa9e14b1623897af61e3d73
sonsungjong <tjdwhd70599@gmail.com>
2023-12-26 13:04:08 +0900

    290문제

---
+++ ConsoleApp/P290_Graph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ConsoleApp
+{
+    public class P290_Graph : Form
+    {
+        private int[] m_arr;
+
+        public static void Main290Graph()
+        {
+            var app = new P290_Graph();
+            Application.Run(app);
+        }
+
+        public P290_Graph()
+        {
+            m_arr = new int[5] { 100, 30, 50, 60, 70 };
+            this.Text = "배열로부터 그래프를 그리는 프로그램";
+            this.Width = 500;
+            this.Height = 250;
+            this.DoubleBuffered = true;
+
+            this.Paint += new PaintEventHandler(fm_Paint);
+        }
+
+        private void fm_Paint(Object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            SolidBrush brush1 = new SolidBrush(Color.Black);
+            SolidBrush brush2 = new SolidBrush(Color.Red);
+            SolidBrush brush3 = new SolidBrush(Color.Blue);
+            SolidBrush brush4 = new SolidBrush(Color.Purple);
+            SolidBrush brush5 = new SolidBrush(Color.Pink);
+            graphics.FillRectangle(brush1, 0, 200- m_arr[0], 50, m_arr[0]);
+            graphics.FillRectangle(brush2, 100, 200- m_arr[1], 50, m_arr[1]);
+            graphics.FillRectangle(brush3, 200, 200- m_arr[2], 50, m_arr[2]);
+            graphics.FillRectangle(brush4, 300, 200- m_arr[3], 50, m_arr[3]);
+            graphics.FillRectangle(brush5, 400, 200- m_arr[4], 50, m_arr[4]);
+        }
+    }
+}
---
+++ ConsoleApp/P290_TimerCircle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace ConsoleApp
+{
+    public class P290_TimerCircle : Form
+    {
+        private Image m_image;
+        private int m_circle_width = 40;
+        private int m_circle_height = 30;
+
+        public static void Main()
+        {
+            var app = new P290_TimerCircle();
+            Application.Run(app);
+        }
+
+        public P290_TimerCircle()
+        {
+            this.Text = "원이 커져가는 프로그램";
+            this.Width = 800;
+            this.Height = 600;
+            this.DoubleBuffered = true;
+            m_image = Image.FromFile("../../../../cafe.png");
+
+            Timer timer = new Timer();
+            timer.Start();
+
+            timer.Tick += new EventHandler(timer_Tick);
+            this.Paint += new PaintEventHandler(fm_Paint);
+        }
+
+        private void timer_Tick(Object sender, EventArgs e)
+        {
+            if(m_circle_height < 300 && m_circle_width < 400)
+            {
+                m_circle_width += 4;
+                m_circle_height += 3;
+            }
+            else
+            {
+                Timer timer = (Timer)sender;
+                timer.Stop();
+            }
+            this.Invalidate();
+        }
+        private void fm_Paint(Object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            GraphicsPath path = new GraphicsPath();
+            Rectangle rect = new Rectangle(0, 0, m_circle_width, m_circle_height);
+            path.AddEllipse(rect);
+            graphics.SetClip(path);
+
+            graphics.DrawImage(m_image, rect, new Rectangle(0, 0, m_circle_width, m_circle_height), GraphicsUnit.Pixel);
+            graphics.ResetClip();
+        }
+    }
+}
