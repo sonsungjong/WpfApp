cf6a1c8cb3c5149b977f8291dcbf3267db5c7745
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2024-01-07 18:39:39 +0900

    C# TCP송수신

---
+++ ConsoleApp/P381_TCPServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp
+{
+    internal class P381_TCPServer
+    {
+        public static string m_host = "localhost";
+        public static int m_port = 10000;               // 포트번호
+        public static void Main381()
+        {
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(m_host);             // IP 대입
+            TcpListener tcpListener = new TcpListener(ipHostEntry.AddressList[0], m_port);              // PORT 대입
+
+            tcpListener.Start();
+
+            Console.WriteLine("대기합니다.");
+
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();            // 접속을 accept해서 받아들인다
+                StreamWriter streamWriter = new StreamWriter(tcpClient.GetStream());
+                streamWriter.WriteLine("이쪽은 서버입니다.");           // 문자열
+                streamWriter.Flush();
+
+                streamWriter.Close();
+                tcpClient.Close();              // 접속을 해제시킨다
+                break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp/P383_TCPClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ConsoleApp
+{
+    internal class P383_TCPClient : Form
+    {
+        public static string m_host = "localhost";
+        public static int m_port = 10000;
+
+        private TextBox m_textBox;
+        private Button m_button;
+
+        public static void Main383()
+        {
+            Application.Run(new P383_TCPClient());
+        }
+
+        public P383_TCPClient()
+        {
+            this.Text = "TCP 클라이언트";
+            this.Width = 300;
+            this.Height = 300;
+
+            m_textBox = new TextBox();
+            m_textBox.Multiline = true;
+            m_textBox.ScrollBars = ScrollBars.Vertical;
+            m_textBox.Height = 150;
+            m_textBox.Dock = DockStyle.Top;
+
+            m_button = new Button();
+            m_button.Text = "접속";
+            m_button.Dock = DockStyle.Bottom;
+
+            m_textBox.Parent = this;
+            m_button.Parent = this;
+
+            m_button.Click += new EventHandler(btn_Click);
+        }
+
+        private void btn_Click(Object sender, EventArgs e)
+        {
+            TcpClient tcpClient = new TcpClient(m_host, m_port);            // 아이피와 포트를 생성자에
+            StreamReader streamReader = new StreamReader(tcpClient.GetStream());
+            String str = streamReader.ReadLine();               // 수신
+
+            m_textBox.Text = str;
+
+            streamReader.Close();
+            tcpClient.Close();              // 접속 해제
+        }
+    }
+}
