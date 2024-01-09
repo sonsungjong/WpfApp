840f5bd0250eac337a542e9945f7420a4ef8553e
tjdwhd70599@gmail.com <tjdwhd70599@gmail.com>
2024-01-09 10:15:51 +0900

    C# OpenFileDialog

--- WPF_CS/image_opf.xaml.cs
+++ WPF_CS/image_opf.xaml.cs
@@ -31,11 +31,13 @@ namespace WPF_CS
             this.image_ctrl.Source = new BitmapImage(image_path);
         }
 
+        // 화면에 가득 채우기
         private void fill_image_button_Click(object sender, RoutedEventArgs e)
         {
             this.image_ctrl.Stretch = System.Windows.Media.Stretch.Fill;
         }
-
+        
+        // 원본유지 맞춤형
         private void zoom_image_button_Click(object sender, RoutedEventArgs e)
         {
             this.image_ctrl.Stretch = System.Windows.Media.Stretch.Uniform;
@@ -44,7 +46,12 @@ namespace WPF_CS
         private void browse_image_button_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
-            openFileDialog.Title = "오픈파일다이얼로그 Select My Image to Show";
+            openFileDialog.Title = "오픈파일다이얼로그 Select My Image to Show";                                     // 창제목
+            openFileDialog.Filter = "제이피지|*.jpg;*.jpeg;*.jfif|png files (*.png)|*.png|모든 파일~|*.*";               // 필터 설정
+            //openFileDialog.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.System);                                      // 초기경로(System32)
+            //openFileDialog.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Fronts);                                      // 초기경로(폰트)
+            //openFileDialog.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);                                      // 초기경로(내 사진)
+            openFileDialog.InitialDirectory = "C:\\Users";                                      // 초기경로
             openFileDialog.ShowDialog();
 
             this.path_label.Content = openFileDialog.FileName;
