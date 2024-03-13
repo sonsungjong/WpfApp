e1da8506d8a03ca788f63644457fd1ec8da683c5
sonsungjong <tbxmtbfm@naver.com>
2024-03-13 17:28:45 +0900

    test

--- ConsoleApp/P999_csv.cs
+++ ConsoleApp/P999_csv.cs
@@ -8,6 +8,13 @@ namespace ConsoleApp
 {
     public class P999_csv
     {
-
+        public static void Main()
+        {
+            byte bb = 27;
+            if(bb == 27)
+            {
+                Console.WriteLine(bb);
+            }
+        }
     }
 }
